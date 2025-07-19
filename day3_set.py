f2d927332288328ead2da1fc733381cb952e49cd
GOdfather <eyasin.113t@gmail.com>
2025-07-19 07:01:23 +0600

    Add: Material on python set

---
+++ day3_set.py
@@ -0,0 +1,24 @@
+#Let's learn SET
+
+#Creating a set 
+#style 01:
+set1= {1,2,3,4,5}
+set2= {"Apple","Banana","Cherry"}
+set3= {False, True}
+set4= {1,2,"Apple", True}
+
+#style: 02
+set5= set((1,2,3,4,5))
+set6= set(("Apple","Banana","Cherry"))
+set7= set((False, True))
+set8= set((1,2,"Apple", True))
+
+#Printing the set
+print(set1)
+print(set2)
+print(set3)
+print(set4)
+print(set5)
+print(set6)
+print(set7)
+print(set8)
